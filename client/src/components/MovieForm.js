38de323978d230cbfef4e6e3aad9356ff3337f32
Michael <michael.daniels1013@gmail.com>
2022-12-14 00:02:29 -0600

    commit tree 2df65add1591cef1568b3ac18dbf6faf8efb3ed1

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -13,6 +13,7 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
+  const [errors, setErrors] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,8 +24,13 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((newMovie) => console.log(newMovie));
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors));
+        }
+      })
   }
 
   function handleChange(e) {
@@ -125,6 +131,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
